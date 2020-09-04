b63cab7f642c3ff6f1dc24ec6da85cf5692733cd
delena671 <69768942+delena671@users.noreply.github.com>
2020-09-03 22:43:48 -0400

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,25 @@
+window.alert("Hello, welcome to my page"); //
+var A = "My Name is Adeline";
+document.write(A);
+document.write("Lisa told Bart, \"Knock it off, Bart! or Ill tell dad!\"<br> \"Eat my shorts!\" Bart responded."); //This is to define a quote properly, even with the excess quotation marks.
+document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\"" + "-Dr. Seuss"); //This is to define a quote properly, even with the excess quotation marks.
+var A = "Concatenated" + " String"
+document.write(A)
+var family = "Gutgohu", dad = "Roque", mom = "Suki", brother1 = "David", brother2 = "Daniel", brother3 = "Joshua"; 
+document.write(mom); 
+var blues = "dad"; //Defining the color of the text "dad"
+var blues = blues.fontcolor ("blue");
+document.write(blues);
+var reds = "mom"; //Defining the color of the text "mom"
+var reds = reds.fontcolor("red");
+document.write(reds);
+var greens = "brother1"; //Defining the color of the text "brother1"
+var greens = greens.fontcolor("green");
+document.write(greens);
+var pinks = "brother2";//Defining the color of the text "brother2"
+var pinks = pinks.fontcolor("pink");
+document.write(pinks);
+var purples = "brother3";//Defining the color of the text "brother3"
+var purples = purples.fontcolor("purples");
+document.write(purples);
+document.write(3+3) //This is combine the numbers together
