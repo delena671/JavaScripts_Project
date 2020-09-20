d08a63d4c76248498d4db23e1a8b7cb4e5480d52
delena671 <69768942+delena671@users.noreply.github.com>
2020-09-20 12:36:36 -0400

    Add files via upload

---
+++ Basic JavaScript Projects/Project7_scope_time_function/main.js
@@ -0,0 +1,65 @@
+var X = 10;
+function Add_numbers_1() {
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function Add_numbers_2() {
+    document.write(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+function Add_numbers_1() {
+    var X = 10;
+    console.log(15 + X);
+}
+function Add_numbers_2() {
+    console.log(X + 100);
+}
+Add_numbers_1();
+Add_numbers_2();
+
+if (6 < 13) {
+    document.write("The left number is smaller than the number on the right.")
+}
+
+function get_Date() {
+    if (new Date().getHours()< 18) {
+        document.getElementById("Greeting").innerHTML = "How are you today?";
+    }
+}
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 18) {
+        Vote = "You are old enough to vote!";
+    }
+    else {
+        Vote = "You are not old enough to vote!";
+    }
+    document.getElementById("How_old_are_you?").innerHTML = Vote;
+}
+
+function time_function() {
+    var Time = new Date ().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >12 == Time <0) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
